49e01a60db9f89a783b5214059ddffca19ecae91
Rodolfo Santos <santoseuro2018@gmail.com>
2020-02-29 20:28:39 -0500

    [Kafka Aula 1 - Atividade 4 Criando produtores em Java Alura .mp4]  Criando mensagens no topico ECOMMERCE_NEW_ORDER e consumindo as mensagens.

--- ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -6,13 +6,24 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
-    public static void main(String[] args) {
-        var producer = new KafkaProducer<String, String>(properties());
-        var record =   new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", "");
-        producer.send(record); // record para ficar registrado no kafka
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties());
+        String value = "12512, 45421, 98655";
+
+        ProducerRecord<String, String> record =
+                new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record, (data, ex) -> {
+            if(ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println( "Sucesso ao enviar mensagem >> " +  data.topic() + "::: partition " +  data.partition()
+                               + " /offset " + data.offset()  + " / timestamp " + data.timestamp());
+        } ).get(); // record para ficar registrado no kafka
     }
 
     private static Properties properties() {
