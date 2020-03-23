aa69ac817f70b127a3f99b0b8f76815b0b37e081
Rodolfo Santos <santoseuro2018@gmail.com>
2020-03-22 21:34:22 -0500

    [FIX] Removendo imports desnecessários

--- ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -1,18 +1,8 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
 public class EmailService {
 
