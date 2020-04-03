5020516b738e2efaa4d3011164ac9c51b48b974b
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-04-03 08:12:14 -0300

    [Curso 2 - Kafka Aula 4 - Atividade 5 Acks e reliability Alura - Cursos]

--- ecommerce/service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ ecommerce/service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -12,23 +12,27 @@ public class NewOrderMain {
 
 //        Independente de dar erro ou não ele vai fechar a conexão atraves do metodo close
 //        da classe KafkaDispatcher
-        try (KafkaDispatcher dispatcher = new KafkaDispatcher<Order>()) {
-                String userId = UUID.randomUUID().toString();
-                String orderId = UUID.randomUUID().toString();
-                BigDecimal amount = new BigDecimal(Math.random() * 5000 + 1);
 
-                String emailCode = "rodolfo@email.com";
-                
-                Order order = new Order(userId, orderId, amount, emailCode);
-                dispatcher.send("ECOMMERCE_NEW_ORDER", emailCode, order);
 
-            try (KafkaDispatcher emailDispatcher = new KafkaDispatcher<Email>()) {
-                //        ENVIO DE EMAIL
-                String key = UUID.randomUUID().toString();
-                String value = "Mensagem : ";
-                String keyValue = value + key;
-                String email = "Thank you for your order! We are processing your order!";
-                emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
+        for (int i=0; i<= 500; i++) {
+            try (KafkaDispatcher dispatcher = new KafkaDispatcher<Order>()) {
+                    String userId = UUID.randomUUID().toString();
+                    String orderId = UUID.randomUUID().toString();
+                    BigDecimal amount = new BigDecimal(Math.random() * 5000 + 1);
+
+                    String emailCode = "rodolfo@email.com";
+
+                    Order order = new Order(userId, orderId, amount, emailCode);
+                    dispatcher.send("ECOMMERCE_NEW_ORDER", emailCode, order);
+
+                try (KafkaDispatcher emailDispatcher = new KafkaDispatcher<Email>()) {
+                    //        ENVIO DE EMAIL
+                    String key = UUID.randomUUID().toString();
+                    String value = "Mensagem : ";
+                    String keyValue = value + key;
+                    String email = "Thank you for your order! We are processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
+                }
             }
         }
     }
