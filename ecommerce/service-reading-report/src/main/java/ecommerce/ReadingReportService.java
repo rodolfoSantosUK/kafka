72145d01ad29e33e385b851b9f78645a9f972b7a
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-04-06 21:34:52 -0300

    [Curso 3 - Kafka Aula 1 - Atividade 2 Simulando a geração de relatórios] Foi criado mais um serviço no projeto

---
+++ ecommerce/service-reading-report/src/main/java/ecommerce/ReadingReportService.java
@@ -0,0 +1,48 @@
+package ecommerce;
+
+import br.com.alura.ecommerce.KafkaConsumerService;
+import br.com.alura.ecommerce.KafkaDispatcher;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
+public class ReadingReportService {
+
+    private final KafkaDispatcher<User> orderKafkaDispatcher = new KafkaDispatcher<>();
+
+    private static final Path SOURCE = new File("src/main/resources/report.txt").toPath();
+
+    public static void main(String[] args) {
+
+        ReadingReportService reportService = new ReadingReportService();
+                                       // Passando o tipo que eu gostaria de desserializar
+        try(KafkaConsumerService service = new KafkaConsumerService<>(  ReadingReportService.class.getSimpleName(),
+                                            "USER_GENERATE_READING_REPORT",
+                                                  reportService :: parse,
+                                                  User.class,
+                                                  new HashMap<>())){
+                                                  service.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, User> rec) throws IOException {
+        System.out.println("Processing record for " + rec.value());
+
+        User user = rec.value();
+        File target =  new File(user.getReportPath());
+        IO.copyTo(SOURCE, target);
+        IO.append(target, "Created for " + user.getUuid());
+
+        System.out.println("File created: " + target.getAbsolutePath());
+    }
+
+
+
+}
